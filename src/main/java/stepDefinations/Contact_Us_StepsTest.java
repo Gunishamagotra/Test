908e3853ef4ff6815aad387fb316537fd2b10935
Gunishamagotra <gunisha.m@gmail.com>
2023-01-24 22:37:17 +0000

    new

--- src/main/java/stepDefinations/Contact_Us_StepsTest.java
+++ src/main/java/stepDefinations/Contact_Us_StepsTest.java
@@ -13,10 +13,17 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 
+import java.util.Random;
+
 public class Contact_Us_StepsTest
 
 {
    private WebDriver driver;
+
+     int generateRandomNumber(int min, int max){
+       Random random= new Random();
+       return random.nextInt(max-min) +min;
+   }
         @Before
                 public void setup()
         {
@@ -46,17 +53,19 @@ public class Contact_Us_StepsTest
     }
     @When("User enters unique first name")
     public void user_enters_unique_first_name() {
-            driver.findElement(By.xpath("//*[@id=\"contact_form\"]/input[1]")).sendKeys("Farzi");
+            driver.findElement(By.xpath("//*[@id=\"contact_form\"]/input[1]"))
+                    .sendKeys("Farzi" + generateRandomNumber(1,10));
 
 
     }
     @And("User enters unique last name")
     public void user_enters_unique_last_name() {
-            driver.findElement(By.xpath("//*[@id=\"contact_form\"]/input[2]")).sendKeys("Sv");
+            driver.findElement(By.xpath("//*[@id=\"contact_form\"]/input[2]"))
+                    .sendKeys("Sv" + generateRandomNumber(1,1000));
     }
     @And("User enters unique email id")
     public void user_enters_unique_email_id() {
-            driver.findElement(By.xpath("//*[@id=\"contact_form\"]/input[3]")).sendKeys("farzisv@gmail.com");
+            driver.findElement(By.xpath("//*[@id=\"contact_form\"]/input[3]")).sendKeys("farzisv" + generateRandomNumber(1,100) + "@gmail.com");
     }
     @And("User enetrs comment")
     public void user_enetrs_comment() {
